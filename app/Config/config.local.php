f6afe660ac604ab17e3b7ba082e1d3269bccb3fb
Carlos Núñez <c2vai2003@me.com>
2025-10-11 20:16:31 -0300

    Sistema legado entero

---
+++ app/Config/config.local.php
@@ -0,0 +1,13 @@
+<?php
+// Copie este archivo a config.local.php y complete sus credenciales reales
+date_default_timezone_set('America/Punta_Arenas');
+
+return [
+    'db' => [
+        'host' => '127.0.0.1:3386',
+        'name' => 'terranova',
+        'user' => 'root',
+        'pass' => '',
+        'charset' => 'utf8mb4',
+    ],
+];
